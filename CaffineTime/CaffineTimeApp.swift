a7a303cba76cbc4e7df4893617a19fafccb2d120
Jaden Hong <jaden@Jadens-MacBook-Air.local>
2023-02-01 18:00:23 -0500

    First initial commit.

---
+++ CaffineTime/CaffineTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffineTimeApp.swift
+//  CaffineTime
+//
+//  Created by Jaden Hong on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct CaffineTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
