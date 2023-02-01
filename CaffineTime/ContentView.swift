3061224d00aa845183b3007673035ab9ad58afee
Jaden Hong <jaden@Jadens-MacBook-Air.local>
2023-02-01 18:26:39 -0500

    SwiftUI. MapKit added.

--- CaffineTime/ContentView.swift
+++ CaffineTime/ContentView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+    )
  
     var body: some View {
+        
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.all)
  
         VStack {
             Image(systemName: "globe")
